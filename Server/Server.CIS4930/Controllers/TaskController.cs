a142aecdc3809e1505eea7ae93a9b06bdf790b11
nmfanguy <62917714+nmfanguy@users.noreply.github.com>
2022-03-02 12:09:42 -0500

    Update Server solution to include TaskController and removed WeatherForecast

---
+++ Server/Server.CIS4930/Controllers/TaskController.cs
@@ -0,0 +1,25 @@
+﻿using Lib.CIS4930.Standard.Models;
+using Lib.CIS4930.Standard.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.CIS4930.Controllers
+{
+    [Route("api/tasks")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private ITaskService taskService;
+
+        public TaskController()
+        {
+            taskService = LocalTaskService.Instance;
+        }
+
+        [HttpGet()]
+        public IEnumerable<ITask> Get()
+        {
+            return taskService.Tasks;
+        }
+    }
+}
